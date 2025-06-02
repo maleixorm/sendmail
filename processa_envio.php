3da8436f885b2fd84fa348f7c425aba3eeb0a3da
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-01 23:20:25 -0300

    Criando a classe mensagem com seus métodos e atributos

--- processa_envio.php
+++ processa_envio.php
@@ -1,3 +1,37 @@
 <?php
 
-print_r($_POST);
+class Mensagem {
+    private $para;
+    private $assunto;
+    private $mensagem;
+
+    function __construct($para, $assunto, $mensagem)
+    {
+        $this->para = $para;
+        $this->assunto = $assunto;
+        $this->mensagem = $mensagem;
+    }
+
+    public function __get($atributo) {
+        return $this->$atributo;
+    }
+
+    public function __set($atributo, $valor) {
+        $this->$atributo = $valor;
+    }
+
+    public function mensagemValida() {
+        if (empty($this->para) || empty($this->assunto) || empty($this->mensagem)) {
+            return false;
+        }
+        return true;
+    }
+}
+
+$mensagem = new Mensagem($_POST['para'], $_POST['assunto'], $_POST['mensagem']);
+
+if ($mensagem->mensagemValida()) {
+    echo 'Mensagem é válida.';
+} else {
+    echo 'Mensagem não é válida';
+}
