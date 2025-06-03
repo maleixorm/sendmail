7c8ce35ee6d0bea91d32e16435621cf888728eed
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-03 10:53:00 -0300

    Atualizando a head da aplicação

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<html>
-	<head>
-		<meta charset="utf-8" />
-    	<title>App Mail Send</title>
-
-    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-	</head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App Send Mail</title>
+   	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
 
 	<body>
 
